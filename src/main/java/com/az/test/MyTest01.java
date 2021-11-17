10f7643c86dd7ff01a5f6b7c7a25fb2cc1acdd67
Azheng <1171209153@qq.com>
2021-11-17 21:53:14 +0800

    second commit.

--- src/main/java/com/az/test/MyTest01.java
+++ src/main/java/com/az/test/MyTest01.java
@@ -4,10 +4,11 @@ public class MyTest01 {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+
 
         System.out.println("This is Change");
-
     }
 
 }
