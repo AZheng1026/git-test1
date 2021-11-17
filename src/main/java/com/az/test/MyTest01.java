6aeeed961cba12773254f10a09e4deeb0c6d5f58
Azheng <1171209153@qq.com>
2021-11-17 21:50:21 +0800

    first commit.

---
+++ src/main/java/com/az/test/MyTest01.java
@@ -0,0 +1,13 @@
+package com.az.test;
+
+public class MyTest01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+        System.out.println("This is Change");
+
+    }
+
+}
